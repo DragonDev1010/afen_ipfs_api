e926df404d2cc5f938064183904b541fdb35149a
DavidYu1010 <provic0099@gmail.com>
2021-06-18 08:16:00 -0700

    set node start script

--- test.js
+++ test.js
@@ -2,10 +2,17 @@ const express = require('express')
 const {create} = require('ipfs-http-client')
 const expFileUpload = require('express-fileupload')
 const fs = require('fs')
+const {ethers} = require('ethers')
 
 const ipfs = create('http://localhost:5001')
 const app = express()
 app.use(expFileUpload())
+
+app.get('/created', (req, res) => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    console.log(provider)
+    res.send(req.query.hash)
+})
 
 app.post('/upload', (req, res) => {
     let fileObj = {}
@@ -13,12 +20,14 @@ app.post('/upload', (req, res) => {
         const file = req.files.inputFile
         const fileName = file.name
         const filePath = __dirname + '/files/' + fileName
+        // Move uploaded file to directory/files/uploadedfilename
+        // directory `files` already have to be created before uploading
         file.mv(filePath, async (err) => {
             if(err) {
                 console.log('Error: failed to download file')
                 return res.status(500).send(err)
             }
-
+            // e.x. cid = `QmRYQK3MxEtxcZhhfE3Ht9uAn6DM5q9rFJ1tx3AKANzrUZ`
             const fileHash = await addFile(fileName, filePath)
             console.log('File Hash received __>', fileHash)
             fs.unlink(filePath, (err) => {
@@ -26,12 +35,11 @@ app.post('/upload', (req, res) => {
                     console.log("Error: Unable to delete file.", err)
                 }
             })
-            fileObj = {
-                file: file,
-                name: fileName,
-                path: filePath
-            }
-            res.send(fileObj)
+            // fileObj = {
+            //     file: file,
+            //     name: fileName,
+            //     path: filePath
+            // }
         })
     } else {
         res.send('file upload false')
@@ -48,7 +56,8 @@ const addFile = async (fileName, filePath) => {
         progress: (len) => console.log('Uploading file ... ' + len)
     })
     console.log(filesAdded)
-    const fileHash = filesAdded.cid.toString
+    const fileHash = filesAdded.cid
+    console.log(fileHash)
     return fileHash
 }
 
